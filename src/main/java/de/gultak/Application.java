caf0150abe7aa05a25b2f9a47fd8b23ccb43fbf5
WAYGuardian <github@gultak.de>
2019-01-18 00:23:38 +0100

    Test

--- src/main/java/de/gultak/Application.java
+++ src/main/java/de/gultak/Application.java
@@ -26,7 +26,7 @@ public class Application {
 
     private static final String visaURL = "https://kiew.diplo.de/blob/1350604/2a61a4399c8a3e3a686419ad5a6797b4/pdf-abholbereite-visa-neu-data.pdf";
 
-    private static final String visaNumberIra = "1901243"; // "1901406";
+    private static final String visaNumberIra = "1901406";
 
     private static final String targetEmail = "visa@gultak.de";
 
